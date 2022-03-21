3747b95d40f3bd1a65d38de0a872bdf9ec81a16a
Your Name <you@example.com>
2022-03-20 22:24:44 -0300

    new try

--- list.c
+++ list.c
@@ -130,7 +130,7 @@ void * popCurrent(List * list) {
 
     if(list->current == NULL) return NULL;
     
-    void datoAux = list->current->data;
+    int datoAux = list->current->data;
 
     if(list->current->prev == NULL)
     {
